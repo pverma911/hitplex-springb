1c9afb1f0fe0eb94541cce4b1e0e35ac13768b43
pverma911 <verma.pranshu689@gmail.com>
2025-04-28 07:20:46 +0530

    initial commit

---
+++ src/main/java/com/movieplex/hitplex/HitplexApplication.java
@@ -0,0 +1,13 @@
+package com.movieplex.hitplex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HitplexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HitplexApplication.class, args);
+	}
+
+}
